77cd970ce62b0abeb2f59cd5664eedf88896353f
kaungsett <rrocket438@gmail.com>
2024-07-17 09:28:27 +0630

    Okay

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // import reactRefresh from '@vitejs/plugin-react-refresh';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: "/Reactportfolio/",
+  plugins: [react()],
 });

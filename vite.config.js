49b1ca88fac4a8d94a94d5219d157593cb410fe6
kaungsett <rrocket438@gmail.com>
2024-07-17 09:31:27 +0630

    reUpload

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // import reactRefresh from '@vitejs/plugin-react-refresh';
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Reactportfolio/",
+  base:'/Reactportfolio/',
   plugins: [react()],
 });

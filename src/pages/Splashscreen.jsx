1546d857f619965e333f870059bc8b548a5ef519
kaungsett <rrocket438@gmail.com>
2024-07-19 19:45:38 +0630

    add animation

--- src/pages/Splashscreen.jsx
+++ src/pages/Splashscreen.jsx
@@ -19,7 +19,7 @@ const container = {
 function SplashMessage() {
   return (
       <motion.div
-      className="flex items-center justify-center min-h-screen font-monst font-extrabold text-lg text-[#1A8B9C] bg-[#CDF7FD]"
+      className="flex items-center justify-center min-h-screen font-monst font-extrabold text-lg text-[#1A8B9C] "
       variants={container}
       initial="hidden"
       animate="visible"

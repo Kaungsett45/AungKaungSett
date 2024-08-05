5d80b11bdc720eebd564097163abce7ee2e582dd
kaungsett <rrocket438@gmail.com>
2024-08-05 19:33:34 +0630

    Blogs update

--- src/Blogs/Blogs.jsx
+++ src/Blogs/Blogs.jsx
@@ -1,31 +1,33 @@
 import React from 'react'
-import bgimg from '../../public/blog.svg'
-import thanks from '/public/thanksgif.gif'
+
+import jsimg from '../../public/reactvirtual.svg'
+import view from '../../public/view.svg'
+import { Link } from 'react-router-dom'
 export default function Blogs() {
   return (
-    <div className="flex flex-col min-h-screen">
-          <div className='my-12 mx-2 flex justify-between items-align border-[#1A8B9C] border-2 p-3 rounded-lg'>
-            
-            <div className='text-right '>
-            <h2 className='text-center font-bold font-mari my-'>🚧 I'm Working on Something Great! 🚧</h2>
-        <p className=' m-2 text-left '>
-            Thank you for visiting this portfolio! i'm currently making some exciting updates to enhance your experience. 
-           
-        </p>
-            </div>
-            
-          </div>
-          {/* <div className='my-4 mx-2 flex justify-between items-align border-[#1A8B9C] border-2 p-3 rounded-lg'>
-            <img src={bgimg} alt="blog-icon" />
-            <div className='text-right '>
-                <h4 className='text-base font-mari font-bolder'>Dududada Song</h4>
-                <span className='text-xs'>27/4/2024</span>
-            </div>
-          </div> */}
-          
-          <div className='flex justify-center my-12'>
-            <img src={thanks} alt="thank you" className='w-52' />
-          </div>
+    <div>
+        <div className='mx-4 my-2 border-2  shadow-lg backdrop-blur-sm rounded-lg drop-shadow-2xl px-2 min-h-[26vh]'>
+              <img src={jsimg} alt="" className='my-2 w-full rounded-lg' />
+                 
+              <div className='flex items-center justify-between mx-2 my-1'>
+                    <div className='flex '>
+                        <img src={view} alt="view" className='h-6 w-5' />
+                        <p>12</p>
+                    </div>
+                  <Link to="" className='bg-[#1A8B9C] px-4 py-1 text-white rounded-full font-mari'>Read</Link>
+              </div>
+        </div>
+        <div className='mx-4 my-2 border-2  shadow-lg backdrop-blur-sm rounded-lg drop-shadow-2xl px-2 min-h-[26vh]'>
+              <img src={jsimg} alt="" className='my-2 w-full rounded-lg' />
+                 
+              <div className='flex items-center justify-between mx-2 my-1'>
+                    <div className='flex items-center justify-center'>
+                        <img src={view} alt="view" className='h-6 w-5' />
+                        <p>12</p>
+                    </div>
+                  <Link to="" className='bg-[#1A8B9C] px-4 py-1 text-white rounded-full font-mari'>Read</Link>
+              </div>
+        </div>
     </div>
   )
 }

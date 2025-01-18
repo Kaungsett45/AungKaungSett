1e759ec394024f80036aefa60f9e9294809984a6
kaungsett <rrocket438@gmail.com>
2025-01-18 14:28:33 +0630

    new update

--- src/component/Experience.jsx
+++ src/component/Experience.jsx
@@ -25,48 +25,49 @@ export default function Experience() {
               style={{ backgroundColor: "#204057", color: "white" }}
             >
               <p className="  font-bold text-sm lg:text-xl ">
-                2024 - Present
+                Code Mal
                 <img src={whitestar} alt="Star" className='absolute right-2 w-3 xs:w-6 '/>
          
                </p>
-              <p className="text-4xsl "
+              <p className="text-lg "
                style={{ color: "#B7B7B7" }}>
-              Content Writer </p>
+              Content Writer -2024 </p>
 
-              <p className="mt-2  text-xsl text-left">
+              <p className="mt-2  text-xsl text-right">
               worked as a volunteer content creator, gaining writing knowledge in IT and related fields, which helped me develop my skills as a content writer </p>
           </div>
           <hr className='w-6 h-1 sm:w-12 sm:h-2 md:w-12 bg-[#204057] border-0'></hr>
-          <div className='px-2 w-24 h-10 xss:w-24 xs:w-32 xs:h-12  sm:h-19 md:h-20  sm:w-24 md:w-32 md:h-18 lg:w-30 lg:h-18 border-4 border-[#204057] bg-[#0148BE] rounded-full flex items-center justify-center overflow-hidden'>
+          <div className='px-2 w-24 h-10 xss:w-24 xs:w-32 xs:h-12  sm:h-19 md:h-20  sm:w-24 md:w-28 md:h-18 lg:w-30 lg:h-18 border-4 border-[#204057] bg-[#0148BE] rounded-full flex items-center justify-center overflow-hidden'>
               <img src={codemal} alt="react" className='w-12 h-8 sm:w-6 sm:h-10 md:w-20 md:h-24 object-contain' />
           </div>
       </div>
 
-      {/* oky */ }
-      <div className='flex items-center my-4 p-2'>
-        <div
-              className=" my-4 p-3 rounded-lg font-mari font-thin  text-xs xs:text-lg xs:p-6 "
+      {/**fronted developer */}
+      <div className=' transform scale-x-[-1]  flex   items-center my-4 p-2'>
+          <div className='px-2 w-24 h-10 xss:w-24 xs:w-32 xs:h-12  sm:h-19 md:h-20  sm:w-24  md:h-18 lg:w-30 lg:h-18 border-4 border-[#204057] bg-[#0148BE] rounded-full flex items-center justify-center overflow-hidden'>
+              <img src={codemal} alt="react" className='w-12 h-8 sm:w-6 sm:h-10 md:w-20 md:h-24 object-contain' />
+          </div>
+           <hr className='w-6 h-1 sm:w-12 sm:h-2 md:w-12 bg-[#204057] border-0'></hr>
+         
+          <div
+              className="transform scale-x-[-1] p-3 my-4  rounded-lg font-mari font-thin  text-xs xs:text-lg xs:p-6 "
               style={{ backgroundColor: "#204057", color: "white" }}
             >
-                 <p className="relative  font-bold text-sm lg:text-xl ">
-                2022 - Present
-             
+              <p className="  font-bold text-sm lg:text-xl ">
+                Code Mal
                 <img src={whitestar} alt="Star" className='absolute right-2 w-3 xs:w-6 '/>
          
                </p>
-              <p className="text-4xsl "
+              <p className="text-lg "
                style={{ color: "#B7B7B7" }}>
-              Web Developer </p>
-              <p className="mt-2  text-xsl text-left">
-              Full-stack developer with React, Java, Tailwind, Node.js, and Firebase skills. I’ve worked on multiple freelance projects and am now seeking new challenges
-              </p>
-               </div>
-          <hr className='w-8 h-1 sm:w-12 sm:h-2 md:w-12 bg-[#204057] border-0'></hr>
-            <div className='px-2 w-24 h-10 xss:w-24 xs:w-32 xs:h-12 sm:w-24 sm:h-19 md:w-36 md:h-20 lg:w-30 lg:h-20 border-4 border-[#204057] rounded-full flex items-center justify-center overflow-hidden'>
-              <img src={react} alt="react" className='mx-1 w-14 h-8 sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-8 lg:h-10 object-contain' />
-            </div>
+              Frontend Developer-2025 </p>
 
+              <p className="mt-2  text-xsl text-left">
+              have gained expertise in creating responsive, user-friendly interfaces using modern technologies.</p>
+           </div>
+          
       </div>
+
   </div>
   
  
--- src/pages/Nav.jsx
+++ src/pages/Nav.jsx
@@ -22,7 +22,7 @@ export default function Navigation(){
             <h2 ><NavLink to="/" className='text-base font-roboto font-extrabold rounded-full  tracking-wide  text-[#204057] py-1 px-3 xs:text-xl sm:text-2xl'>PORTFOLIO</NavLink></h2>
             <div className=' flex  items-center items-align'>
                 <p className='mx-4 font-bold font-roboto text-lg xs:text-xl sm:mx-8 '><NavLink to="/blogs">Blogs</NavLink></p>
-                <Link to= "https://drive.google.com/file/d/1s3RxfT8VOWmW8BNIM5YyQeB-lXlFZwnC/view?usp=sharing" target="_blank">
+                <Link to= "https://drive.google.com/file/d/16oTYIA3kZ4k-uCJyoT-uSv5p66g7yD_l/view?usp=drive_link" target="_blank">
                 <button className='
                 text-sm font-roboto font-bold text-white  py-1 px-2 rounded-full border-4 border-[#204057] xs:border-6 xs:px-4
                 sm:border-7 sm:px-6 sm:text-lg

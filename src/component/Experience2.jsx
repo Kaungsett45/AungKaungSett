ef6ec91cf2abfaccc6a53a63169183cd1d60d343
kaungsett <rrocket438@gmail.com>
2025-02-02 21:52:31 +0630

    push experiences ui update :fix:

--- src/component/Experience2.jsx
+++ src/component/Experience2.jsx
@@ -13,20 +13,22 @@ export default function Experience2() {
         </div>
 
         {/**Code Mal */}
-        <div className='p-4 '>
+        <div className='md:p-8 p-4 '>
             <div className='flex items-center'>
-                 <img className="w-10 h-10 rounded-sm" src={codemal} alt="Default avatar"/>
-                <h2 className='font-monst mx-2 font-semibold xs:text-md '>Code Mal</h2>
+                 <img className="w-10 h-10 md:w-16 md:h-16 rounded-sm" src={codemal} alt="Default avatar"/>
+                <h2 className='font-monst mx-2 font-semibold xs:text-md  md:text-xl '>Code Mal</h2>
             </div>
-            <div className='mt-2  font-semibold text-sm'>
-                <p className='m-1'>🛠 Frontend Developer  </p>
-                <p className='m-1'>📍 2024 - Present </p>
-
-                <p className='text-left leading-4 border-2 p-2 rounded-md mt-4 text-xs
-                dark:shadow-[0_0_6px_rgba(26,139,156,0.4)] dark:hover:shadow-[0_0_10px_rgba(26,139,156,0.6)]
-                
-                '>Developed UI components and improved user experience for nonprofit websites.
-                    Wrote technical content and guides for developers.</p>
+            <div className='mt-2  xs:text-base md:text-lg lg:text-xl'>
+                <p className='m-1 font-semibold'>🛠  Frontend Developer  </p>
+                <p className='m-1 font-semibold'>📍 2024 - Present </p>
+            <div className="text-left leading-4 border-2 p-2 rounded-md mt-4 
+               dark:shadow-[0_0_6px_rgba(26,139,156,0.4)] dark:hover:shadow-[0_0_10px_rgba(26,139,156,0.6)]
+                 "> 
+               <ul className='lg:ml-6 ml-4 list-disc md:text-lg lg:text-xl xss:text-xs   xs:text-sm'>
+                    <li > Wrote technical content and guides for developers as content writer .</li>
+                    <li>Developed UI components and improved user experience for nonprofit websites.</li>
+                </ul>
+                </div>
             </div>
         </div>
     </>
